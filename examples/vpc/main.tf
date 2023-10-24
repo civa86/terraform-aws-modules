e7143d4f6e6006961777ef44b94e3038535da3db
Dario Civallero <dario.civallero@gmail.com>
2023-10-24 12:21:21 +0200

    example of vpc

---
+++ examples/vpc/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.67"
+    }
+  }
+}
+
+locals {
+  project = "test"
+  env     = terraform.workspace
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      project = local.project
+      env     = local.env
+    }
+  }
+}
+
+module "vpc" {
+  source             = "../../modules/vpc"
+  project            = local.project
+  env                = local.env
+  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
